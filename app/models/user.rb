b5061ce94946dd73d1fdfdcb6fc33d9c87ea6ed0
Hillary <hillfeller@gmail.com>
2015-11-10 18:02:34 -0500

    Third commit, slight changes made.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   def show
+     @user = User.find(params[:id]) if params[:id].present?
+     unless @user
+       @user = current_user
+     end
+   end
 end
