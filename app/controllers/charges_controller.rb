68a21dfd0d07a95166c5f3ff1a34843856023c92
Hillary <hillfeller@gmail.com>
2015-11-13 14:18:44 -0500

    Accounts can now be downgraded

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,32 +4,44 @@ class ChargesController < ApplicationController
     @stripe_btn_data = {
       key: "#{ Rails.configuration.stripe[:publishable_key] }",
       description: "BigMoney Membership - #{current_user.name}",
-      amount: 10_00
+      amount: 15_00
+    }
+
+    @stripe_btn_data2 = {
+      key: "#{ Rails.configuration.stripe[:publishable_key] }",
+      description: "BigMoney Membership Downgrade- #{current_user.name}",
+      amount: 0_00
     }
   end
 
+
+
   def create
+    if current_user.role == "standard"
+      customer = Stripe::Customer.create(
+        email: current_user.email,
+        card: params[:stripeToken]
+      )
 
-    customer = Stripe::Customer.create(
-      email: current_user.email,
-      card: params[:stripeToken]
-    )
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: 15_00,
+        description: "BigMoney Membership - #{current_user.email}",
+        currency: 'usd'
+      )
 
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: 10_00,
-      description: "BigMoney Membership - #{current_user.email}",
-      currency: 'usd'
-    )
+      current_user.premium!
+      flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
+      redirect_to user_path(current_user)
 
-    current_user.premium!
+    elsif current_user.role == "premium"
+      current_user.standard!
+      redirect_to user_path(current_user)
 
-    # set your premium role
+    else
+      redirect_to user_path(current_user)
 
-    flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
-
-    redirect_to user_path(current_user)
-
+    end
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
